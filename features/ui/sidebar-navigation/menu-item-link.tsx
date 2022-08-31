1c73c732b9f9ff9b709110ee475cf6738b195ea5
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-31 15:29:35 +0930

    Add CSS rotation expression to rotate sidebar arrow on click

--- features/ui/sidebar-navigation/menu-item-link.tsx
+++ features/ui/sidebar-navigation/menu-item-link.tsx
@@ -33,7 +33,7 @@ export const Anchor = styled.a`
   text-decoration: none;
 `;
 
-export const Icon = styled.img`
+export const Icon = styled.img<{ isCollapsed?: boolean }>`
   width: ${space(6)};
   margin-right: ${space(3)};
 `;
