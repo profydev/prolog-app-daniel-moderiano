1e92a44e62eb5a0d370f1fcfb5f2bfcb13b3eb41
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-02 17:39:52 +0930

    Sidebar Navigation: Support button should open the user’s mail app (#4)
    * Add mailto functionality to support btn with Cypress test coverage
    
    * Improve comment descriptions
    
    * Fix comment typo

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -24,6 +24,19 @@ describe("Sidebar Navigation", () => {
 
       cy.get("nav").contains("Settings").click();
       cy.url().should("eq", "http://localhost:3000/dashboard/settings");
+
+      // Get window object to access it methods for stubs
+      cy.window().then((win) => {
+        // alias window.open function so it can be referenced later with '@open'
+        cy.stub(win, "open").as("open");
+      });
+
+      // This opens the new window (mail client) and should trigger the stubbed window.open method
+      cy.get("nav").contains("Support").click();
+      cy.get("@open").should(
+        "be.always.calledWith",
+        "mailto:support@prolog-app.com?subject=Support Request:"
+      );
     });
 
     it("is collapsible", () => {
