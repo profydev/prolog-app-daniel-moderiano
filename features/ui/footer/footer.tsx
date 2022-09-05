69906d1f5a706f3efac7dbf39ee49fab01fd12b0
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-05 12:40:01 +0930

    Set Footer component height using theme vars

--- features/ui/footer/footer.tsx
+++ features/ui/footer/footer.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { color } from "@styles/theme";
 
 const Container = styled.footer`
-  height: 60px;
+  height: ${({ theme }) => theme.size.footerHeight};
   /* margin-top: 2rem; */
   background-color: ${color("gray", 50)};
   color: ${color("gray", 500)};
-  flex-grow: 1;
+  /* flex-grow: 1; */
 `;
 
 export function Footer() {
