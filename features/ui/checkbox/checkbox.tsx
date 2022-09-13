b8608b86a2fb3bd7f9dda4d99c11ff67618cc995
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-13 10:59:04 +0930

    Add intial custom checkbox styling for medium style checkbox

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,3 +1,6 @@
+import { color, space } from "@styles/theme";
+import styled from "styled-components";
+
 export enum CheckboxSize {
   sm = "sm",
   md = "md",
@@ -17,6 +20,33 @@ interface CheckboxProps {
   size: CheckboxSize;
 }
 
+const Box = styled.input`
+  /* Hide the native checkbox */
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  margin: 0;
+
+  /* Add custom checkbox styling */
+  background-color: ${color("primary", 50)};
+  padding: 0;
+  height: ${space(5)};
+  width: ${space(5)};
+  border: 1px solid ${color("primary", 600)};
+  border-radius: 6px;
+`;
+
+const Checkmark = styled.svg`
+  position: absolute;
+  left: ${space(1)};
+`;
+
+const Label = styled.label`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
 export function Checkbox({
   label,
   name,
@@ -26,14 +56,31 @@ export function Checkbox({
 }: CheckboxProps) {
   return (
     <div>
-      <label htmlFor={name}>{label}</label>
-      <input
-        type="checkbox"
-        name={name}
-        id={name}
-        disabled={disabled}
-        checked={checkboxState === CheckboxState.checked ? true : false}
-      />
+      <Label htmlFor={name}>
+        <Box
+          type="checkbox"
+          name={name}
+          id={name}
+          disabled={disabled}
+          checked={checkboxState === CheckboxState.checked ? true : false}
+        />
+        <Checkmark
+          width="12"
+          height="9"
+          viewBox="0 0 12 9"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.6666 1.5L4.24992 7.91667L1.33325 5"
+            stroke="#7F56D9"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </Checkmark>
+        <span>{label}</span>
+      </Label>
     </div>
   );
 }
