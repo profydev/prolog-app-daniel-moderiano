6baf8fda68c7f56b00be799ab0c7f10deffc484a
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-23 19:49:19 +0930

    Add options to both select components, ensuring they use the API-compatible values

--- features/issues/components/issue-filters/issue-filters.tsx
+++ features/issues/components/issue-filters/issue-filters.tsx
@@ -1,5 +1,22 @@
+import { IssueLevel, IssueStatus } from "@features/issues/types/issue.types";
 import { Input, SelectComponent } from "@features/ui";
 import styled from "styled-components";
+
+interface OptionType {
+  label: string;
+  value: string;
+}
+
+const statusOptions: OptionType[] = [
+  { label: "Resolved", value: IssueStatus.resolved },
+  { label: "Unresolved", value: IssueStatus.open },
+];
+
+const levelOptions: OptionType[] = [
+  { label: "Info", value: IssueLevel.info },
+  { label: "Warning", value: IssueLevel.warning },
+  { label: "Error", value: IssueLevel.error },
+];
 
 const Container = styled.div`
   display: flex;
@@ -9,8 +26,8 @@ const Container = styled.div`
 export function IssueFilters() {
   return (
     <Container>
-      <SelectComponent placeholder="Status" />
-      <SelectComponent placeholder="Level" />
+      <SelectComponent placeholder="Status" options={statusOptions} />
+      <SelectComponent placeholder="Level" options={levelOptions} />
       <Input placeholder="Project Name" iconSrc="/icons/search.svg" />
     </Container>
   );
