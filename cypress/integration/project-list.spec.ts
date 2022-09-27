44f882aebd4e71405561a7e2ae5e9f968ca80398
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-27 21:58:22 +0930

    Project issue links (#23)
    * Add cypress test for project links to filtered issues pages
    
    * Add dynamic links to filtered issues, and update test with URL-friendly string
    
    * Convert filter to lowercase otherwise incorrect results are returned

--- cypress/integration/project-list.spec.ts
+++ cypress/integration/project-list.spec.ts
@@ -35,7 +35,13 @@ describe("Project List", () => {
           cy.wrap($el).contains(statusNames[index]);
           cy.wrap($el)
             .find("a")
-            .should("have.attr", "href", "/dashboard/issues");
+            .should(
+              "have.attr",
+              "href",
+              `/dashboard/issues?project=${mockProjects[index].name
+                .toLowerCase()
+                .replace(/ /g, "%20")}`
+            );
         });
     });
   });
