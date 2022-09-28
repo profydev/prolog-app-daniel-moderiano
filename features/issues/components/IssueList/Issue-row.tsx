ce347d1d3df116dd02077ddebb4f8cc9683206b6
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-09-28 17:54:49 +0930

    Add styling to 'split' rows of table on mobile

--- features/issues/components/IssueList/Issue-row.tsx
+++ features/issues/components/IssueList/Issue-row.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import capitalize from "lodash/capitalize";
-import { color, space, textFont } from "@styles/theme";
+import { breakpoint, color, space, textFont } from "@styles/theme";
 import { Badge, BadgeColor, BadgeSize } from "@features/ui";
 import { IssueLevel } from "../../types/issue.types";
 import { ProjectLanguage } from "@features/projects";
@@ -18,8 +18,25 @@ const levelColors = {
 };
 
 const Row = styled.tr`
-  &:nth-child(2n) {
-    background: ${color("gray", 50)};
+  border: 1px solid ${color("gray", 200)};
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  border-collapse: separate;
+  border-radius: ${space(2)};
+  border-spacing: 0;
+  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
+    0px 1px 2px rgba(16, 24, 40, 0.06);
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    border: none;
+    display: table-row;
+    margin-bottom: 0;
+    box-shadow: none;
+
+    &:nth-child(2n) {
+      background: ${color("gray", 50)};
+    }
   }
 `;
 
@@ -27,10 +44,6 @@ const Cell = styled.td`
   padding: ${space(4, 6)};
   color: ${color("gray", 500)};
   ${textFont("sm", "regular")}
-  &:nth-of-type(3) {
-    border-radius: 6px;
-    width: 100%;
-  }
 `;
 
 const IssueCell = styled(Cell)`
